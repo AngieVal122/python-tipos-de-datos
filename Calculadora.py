4a9a91caa88c70b1b3e862f2dfef220d1013fc08
AngieVal122 <angiev.ibagueb@konradlorenz.edu.coi>
2023-08-15 14:21:17 -0500

    Residuo y potencia Pyton

--- Calculadora.py
+++ Calculadora.py
@@ -3,7 +3,7 @@ print("Ingrese el número 1")
 numero1 = int(input())
 print("Ingrese el número 2")
 numero2 = int(input())
-print("Ingrese la operación (+, -, *, /)")
+print("Ingrese la operación (+, -, *, /, %,^)")
 operacion = input()
 
 match operacion:
@@ -23,5 +23,17 @@ match operacion:
         # Operación división:
         division = numero1 / numero2
         print("La división es " + str(division))
+
+
+    case '%':
+         residuo = numero1 % numero2
+         print ( " el residuo es" + str (residuo))
+
+    case '^':
+         #modulo
+        potencia = pow(numero1, numero2)
+        print ( " la potencia es " + str (potencia))
+
     case _ :
-        print("Operación inválida")
+        print ( " operaciópn invalida" )
+
